fd0bb0842f98edc7c0a3425eb5e1526393a274af
m1ole <miikka.huotari@metropolia.fi>
2021-09-02 18:33:02 +0300

    First App whit styling

--- App.js
+++ App.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
   },
   droidSafeArea: {
     flex: 1,
-    backgroundColor: '#ccc',
+    backgroundColor: 'rgb(36,40,52)',
     paddingTop: Platform.OS === 'android' ? 30 : 0,
   },
 });

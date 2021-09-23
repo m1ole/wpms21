53bcf4345067b83613a77f0592c9e9f0c2942fe8
m1ole <miikka.huotari@metropolia.fi>
2021-09-23 11:11:07 +0300

    Modified video player

--- views/Single.js
+++ views/Single.js
@@ -1,39 +1,106 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {StyleSheet, ActivityIndicator} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, ActivityIndicator, TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
-import {Card, ListItem, Text} from 'react-native-elements';
+import {Card, ListItem, Text, Icon, Avatar} from 'react-native-elements';
 import {Video, Audio} from 'expo-av';
-import {useUser} from '../hooks/ApiHooks';
+import {useTag, useUser} from '../hooks/ApiHooks';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {formatDate} from '../utils/dateFunctions';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 const Single = ({route}) => {
   const {params} = route;
   const {getUserInfo} = useUser();
   const [ownerInfo, setOwnerInfo] = useState({username: ''});
-  const videoRef = useRef(null);
+  const [videoRef, setVideoRef] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const {getFilesByTag} = useTag();
+  const [avatar, setAvatar] = useState('http://placekitten.com/100');
+
+  const handleVideoRef = (component) => {
+    setVideoRef(component);
+  };
+
+  const unlock = async () => {
+    try {
+      await ScreenOrientation.unlockAsync();
+    } catch (error) {
+      console.error('unlock', error.message);
+    }
+  };
+
+  const lock = async () => {
+    try {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.PORTRAIT_UP
+      );
+    } catch (error) {
+      console.error('lock', error.message);
+    }
+  };
+
+  const showVideoInFullscreen = async () => {
+    try {
+      if (videoRef) await videoRef.presentFullscreenPlayer();
+    } catch (error) {
+      console.error('fullscreen', error.message);
+    }
+  };
+
+  useEffect(() => {
+    unlock();
+
+    const orientSub = ScreenOrientation.addOrientationChangeListener((evt) => {
+      console.log('orientation', evt);
+      if (evt.orientationInfo.orientation > 2) {
+        showVideoInFullscreen();
+      }
+    });
+    return () => {
+      ScreenOrientation.removeOrientationChangeListener(orientSub);
+      lock();
+    };
+  }, [videoRef]);
 
   const getOwnerInfo = async () => {
+    const token = await AsyncStorage.getItem('userToken');
+    setOwnerInfo(await getUserInfo(params.user_id, token));
+  };
+
+  const getAvatar = async () => {
     try {
-      const token = await AsyncStorage.getItem('userToken');
-      setOwnerInfo(await getUserInfo(params.user_id, token));
-      console.log('getOwnerinfo', ownerInfo);
-    } catch (e) {
-      console.log('getOwnerinfo', e.message);
+      const avatarList = await getFilesByTag('avatar_' + params.user_id);
+      if (avatarList.length > 0) {
+        setAvatar(uploadsUrl + avatarList.pop().filename);
+      }
+    } catch (error) {
+      console.error(error.message);
     }
   };
 
   useEffect(() => {
     getOwnerInfo();
+    getAvatar();
   }, []);
 
   return (
     <Card>
-      <Card.Title h4>{params.title}</Card.Title>
-      <Card.Title>
-        {formatDate(new Date(params.time_added), 'HH.mm eeee d. MMMM y')}
-      </Card.Title>
+      <ListItem>
+        {params.media_type === 'image' && <Icon name="image" type="ionicon" />}
+        {params.media_type === 'video' && (
+          <Icon name="videocam" type="ionicon" />
+        )}
+        <ListItem.Content>
+          <ListItem.Title>{params.title}</ListItem.Title>
+          <ListItem.Subtitle>
+            {formatDate(new Date(params.time_added), 'eeee d. MMMM y')}
+          </ListItem.Subtitle>
+          <ListItem.Subtitle>
+            klo {formatDate(new Date(params.time_added), 'HH.mm')}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
       <Card.Divider />
       {params.media_type === 'image' && (
         <Card.Image
@@ -43,15 +110,23 @@ const Single = ({route}) => {
         />
       )}
       {params.media_type === 'video' && (
-        <Video
-          ref={videoRef}
-          style={styles.image}
-          source={{uri: uploadsUrl + params.filename}}
-          useNativeControls
-          resizeMode="contain"
-          usePoster
-          posterSource={{uri: uploadsUrl + params.screenshot}}
-        ></Video>
+        <TouchableOpacity
+          disabled={disabled}
+          onPress={() => {
+            videoRef.playAsync();
+            setDisabled(true);
+          }}
+        >
+          <Video
+            ref={handleVideoRef}
+            style={styles.image}
+            source={{uri: uploadsUrl + params.filename}}
+            useNativeControls
+            resizeMode="contain"
+            usePoster
+            posterSource={{uri: uploadsUrl + params.screenshot}}
+          />
+        </TouchableOpacity>
       )}
       {params.media_type === 'audio' && (
         <>
@@ -62,6 +137,7 @@ const Single = ({route}) => {
       <Card.Divider />
       <Text style={styles.description}>{params.description}</Text>
       <ListItem>
+        <Avatar source={{uri: avatar}} />
         <Text>{ownerInfo.username}</Text>
       </ListItem>
     </Card>

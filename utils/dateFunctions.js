aae90c251ec4177770fd39796f32abc5d07360b5
m1ole <miikka.huotari@metropolia.fi>
2021-09-27 14:11:01 +0300

    Added timesince to list item

--- utils/dateFunctions.js
+++ utils/dateFunctions.js
@@ -1,4 +1,4 @@
-import {format} from 'date-fns';
+import {format, formatDistance} from 'date-fns';
 import {fi} from 'date-fns/locale';
 
 const formatDate = (date, formatStr = 'PP', locale = fi) => {
@@ -7,4 +7,8 @@ const formatDate = (date, formatStr = 'PP', locale = fi) => {
   });
 };
 
-export {formatDate};
+const timeSince = (date, locale = fi) => {
+  return formatDistance(new Date(date), new Date(), {addSuffix: true, locale});
+};
+
+export {formatDate, timeSince};
